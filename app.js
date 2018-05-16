f1a994184e2ea3aeae458a0e3041116737055e13
Jacob <jakexmueller@gmail.com>
2018-05-16 13:34:02 -0500

    cleaning up js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-"use strict"
+//"use strict"
+	
+	
+
+
+console.log('aaabbbbbccccaacccbbbaaabbbaaa')
+stringCompression('aaabbbbbccccaacccbbbaaabbbaaa')
+function stringCompression (str) {
+  
+  var output = '';
+  var count = 0;
+  for (var i = 0; i < str.length; i++) {
+    count++;
+    if (str[i] != str[i+1]) {
+      output += count + str[i];
+      count = 0;
+    }
+  }
+  console.log(output);
+}
